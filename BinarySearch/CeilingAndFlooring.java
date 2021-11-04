7f9836ebde08b8659bed4d08f2f675c8010a5cc2
Ashwin Kumar R <ashwinrkumar56@gmail.com>
2021-11-04 21:03:13 +0530

    01

--- BinarySearch/CeilingAndFlooring.java
+++ BinarySearch/CeilingAndFlooring.java
@@ -3,7 +3,7 @@ package BinarySearch;
 public class CeilingAndFlooring {
 	public static void main(String[] args) {
 		int[] arr = {2,3,5,6,9,11,13,15};
-		int target = 11;
+		int target = 10;
 		int ceilResult = ceiling(arr, target);
 		if(ceilResult >=0) {
 			System.out.println(arr[ceilResult]);
@@ -24,7 +24,7 @@ public class CeilingAndFlooring {
 			else low = mid+1;
 		}
 		
-		return high;
+		return low;
 	}
 	private static int flooring(int[] arr, int target) {
 		int low = 0, high = arr.length-1;
@@ -35,6 +35,6 @@ public class CeilingAndFlooring {
 			else low = mid+1;
 		}
 			
-			return low;
+			return high;
 	}
 }
