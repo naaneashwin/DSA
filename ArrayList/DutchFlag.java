f3c72b4e1891e99fe56923458aee55c901672163
Ashwin Kumar R <ashwinrkumar56@gmail.com>
2021-11-12 18:07:10 +0530

    Code Update

--- ArrayList/DutchFlag.java
+++ ArrayList/DutchFlag.java
@@ -3,7 +3,7 @@ package ArrayList;
 public class DutchFlag {
 	//bring 0 to front, 1 to middle and 2 to end in the same array
 	public static void main(String[] args) {
-		int[] arr = {1,2,0,1,2,0,0,2,1};
+		int[] arr = {2,0,1,2,0,0,2,2};
 		int start=0, middle=0, end=arr.length-1;
 		while(middle<=end) {
 			switch(arr[middle]) {
@@ -12,6 +12,7 @@ public class DutchFlag {
 					arr[middle]=arr[start];
 					arr[start]=temp;
 					start++;
+					middle++;
 					break;
 				}
 				case 1:{
@@ -29,5 +30,34 @@ public class DutchFlag {
 		}
 
 		for(int i: arr) System.out.print(i+" ");
+		System.out.println();
+		int[] a = {0,2,2,1,1,0,0,2,1};
+		int[] b = new int[a.length];
+		b=dutch(a,a.length);
+		for(int i: b) System.out.print(i+" ");
+	}
+	private static int[] dutch(int[] arr, int n) {
+		int low = 0;
+		int high = n-1;
+		int mid = 0 ,temp;
+			while(mid<=high) {
+				if(arr[mid] == 0) {
+					temp = arr[low];
+					arr[low] = arr[mid];
+					arr[mid] = temp;
+					low++;
+					mid++;
+				}
+				else if(arr[mid]==1) {
+					mid++;
+				}
+				else {
+					temp = arr[mid];
+					arr[mid] = arr[high];
+					arr[high] = temp;
+					high--;
+				}
+		}
+		return arr;
 	}
 }
